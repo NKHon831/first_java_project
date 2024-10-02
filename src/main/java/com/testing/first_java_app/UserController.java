6ab0da61563f4876fea4479cdf986ccec2da3b95
NKHon831 <khaihon030831@gmail.com>
2024-10-02 22:33:10 +0800

    java project setup: spring boot

---
+++ src/main/java/com/testing/first_java_app/UserController.java
@@ -0,0 +1,13 @@
+package com.testing.first_java_app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+    @RequestMapping("/")
+    public String homePage(){
+        return "Hello world";
+    }
+}
